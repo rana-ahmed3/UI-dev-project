3397ef221b83c279d24fb4fbbda96b0fa4d25dba
Arwetty <s-arwa.allam@zewailcity.edu.eg>
2025-11-04 22:02:39 +0200

    update: fixed UI and added new styles

---
+++ backup/tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+    darkMode: 'class',
+    content: ["./SRC/**/*.html"],
+    theme: {
+        extend: {
+            colors: {
+                primary: "#13ec5b",
+                "background-light": "#f6f8f6",
+                "background-dark": "#102216",
+            },
+            fontFamily: {
+                display: ["Manrope", "sans-serif"]
+            },
+            borderRadius: {
+                'DEFAULT': '0.25rem',
+                'lg': '0.5rem',
+                'xl': '0.75rem',
+                'full': '9999px'
+            }
+        },
+    },
+    plugins: [],
+}
