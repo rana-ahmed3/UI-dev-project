f6ca51e0504a84d46acd62c47dd9286f3a5513a4
Mostafa Hisham <s-mostafa.ahmed@zewailcity.edu.eg>
2025-12-02 23:38:42 +0200

    Fix: Use Navbar in Layout component and fix sticky/blurred styling for all pages

--- src/components/common/Layout.jsx
+++ src/components/common/Layout.jsx
@@ -1,17 +1,15 @@
 import React from 'react';
-import Header from './Header';
+import Navbar from '../Navbar';
 import Footer from './Footer';
 
 const Layout = ({ children }) => {
     return (
-        <div className="relative flex h-auto min-h-screen w-full flex-col text-slate-800 overflow-x-hidden">
-            <div className="layout-container flex h-full grow flex-col">
-                <Header />
-                <main className="flex-1">
-                    {children}
-                </main>
-                <Footer />
-            </div>
+        <div className="min-h-screen flex flex-col bg-white text-gray-800">
+            <Navbar />
+            <main className="flex-1">
+                {children}
+            </main>
+            <Footer />
         </div>
     );
 };
